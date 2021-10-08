5c5bd20dcb20fe70a45010b4adceb8e0e13edd7d
Lorenz-Spreen <lorenz-spreen@mpib-berlin.mpg.de>
2021-10-08 17:16:57 +0200

    added extra references from other reviews

--- figure2.R
+++ figure2.R
@@ -5,14 +5,13 @@ library(ggplot2)
 library(readr)
 library(dplyr)
 
-data_effect<- read_excel("Data_effects_complete.xlsx")
-
+data_effect<- read_excel("Data_effects_complete.xlsx") 
 
 ######################################################################
 # effect distribution, change outcome for different plots
 
 data_trust<- data_effect %>%
-  filter(outcome_clean == "trust")
+  filter(outcome_clean == "exposure")
 
 gg_b <- ggplot_build(
   ggplot() + geom_histogram(aes(x = data_trust$effect), binwidth=1., stat="count")
@@ -20,7 +19,7 @@ gg_b <- ggplot_build(
 
 nu_bins <- dim(gg_b$data[[1]])[1]
 
-p <- ggplot() + geom_histogram(aes(x = data_trust$effect), binwidth=1.,  fill = c("#7EAB55", "grey", "#EE847D"), stat="count") + 
+p <- ggplot() + geom_histogram(aes(x = data_trust$effect), binwidth=1.,  fill = c("#EE847D", "grey", "#7EAB55"), stat="count") + 
   theme_minimal() + 
   geom_bar(stat='identity', color="black") +
   theme(axis.text.x = element_text(size = 20),
@@ -37,7 +36,7 @@ p
 # distrubution above split up via methods, change outcome for different plots
 
 data_trust<- data_effect %>%
-  filter(outcome_clean == "network/echo chamber")
+  filter(outcome_clean == "exposure")
 
 trust_effects <- data_trust %>%
   dplyr::count(effect, method_clean) %>%
@@ -50,7 +49,7 @@ trust_effects <- trust_effects %>%
 p <- ggplot(trust_effects, aes(fill=effect, y=n, x=method_clean)) + 
   geom_bar(position='stack', stat='identity', color="black") +
   theme_minimal()+
-  scale_fill_manual(values = c("#7EAB55", "grey", "#EE847D")) +
+  scale_fill_manual(values = c("#EE847D", "grey", "#7EAB55")) +
   scale_x_discrete('Method', limits=c("descriptive", "experiment", "network", "panel", "qualitative", "social_media", "social_media_combined", "survey", "survey_combined", "tracking"), drop=FALSE) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 15),
         axis.text.y = element_text(size = 15),
